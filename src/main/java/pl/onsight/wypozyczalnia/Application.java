c5a8c6cd4ffa6bdbb9b1b80895d1ddc74ea6d591
Michał Basiński <michal.basinski88@gmail.com>
2019-01-29 10:46:34 +0100

    enable h2

--- src/main/java/pl/onsight/wypozyczalnia/Application.java
+++ src/main/java/pl/onsight/wypozyczalnia/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        System.setProperty("spring.profiles.active", "mysql");
+        System.setProperty("spring.profiles.active", "default");
 
         SpringApplication.run(Application.class, args);
     }
